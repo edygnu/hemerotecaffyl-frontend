8d509e0462937c2e5119feb53cccd31e919aa7d5
edygnu <edygnu@gmail.com>
2023-04-23 22:58:44 -0300

    empezando react

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = (props) => {
+    return (
+        <>
+            <p>Diseñado y desarrollado por Prof. Emmanuel Ponce de León - &copy;2023</p>
+        </>
+
+    );
+}
+
+export default Footer;
